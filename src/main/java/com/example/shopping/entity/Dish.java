82b3913ce1fbd02d55519d7f3f7925a807a41907
saya <saya.ishizawa@gmail.com>
2023-11-15 10:32:58 +0900

    JavaScriptファイルの分割

--- src/main/java/com/example/shopping/entity/Dish.java
+++ src/main/java/com/example/shopping/entity/Dish.java
@@ -30,16 +30,16 @@ public class Dish {
 	private LocalDate updatedDate;
 
 	// 料理の材料Id
-	//@Column
-	//private long ingredientId;
+	@Column
+	private long ingredientId;
 
 	// 料理の材料名
-	//@Column
-	//private String ingredientName;
+	@Column
+	private String ingredientName;
 
 	// 材料の分量
-	//@Column
-	//private String quantity;
+	@Column
+	private String quantity;
 
 	// 調味料のId
 	@Column
@@ -49,8 +49,8 @@ public class Dish {
 	@Column
 	private String seasoningName;
 	
-	@OneToMany(mappedBy="ingredient")
-	private Ingredient ingredient;
+	//@OneToMany(mappedBy="ingredient")
+	//private Ingredient ingredient;
 	
 	public long getDishId() {
 		return dishId;
@@ -92,29 +92,29 @@ public class Dish {
 		this.updatedDate = updatedDate;
 	}
 
-	//public long getIngredientId() {
-		//return ingredientId;
-	//}
+	public long getIngredientId() {
+		return ingredientId;
+	}
 
-	//public void setIngredientId(long ingredientId) {
-		//this.ingredientId = ingredientId;
-	//}
+	public void setIngredientId(long ingredientId) {
+		this.ingredientId = ingredientId;
+	}
 
-	//public String getIngredientName() {
-		//return ingredientName;
-	//}
+	public String getIngredientName() {
+		return ingredientName;
+	}
 
-	//public void setIngredientName(String ingredientName) {
-		//this.ingredientName = ingredientName;
-	//}
+	public void setIngredientName(String ingredientName) {
+		this.ingredientName = ingredientName;
+	}
 
-	//public String getQuantity() {
-		//return quantity;
-	//}
+	public String getQuantity() {
+		return quantity;
+	}
 
-	//public void setQuantity(String quantity) {
-		//this.quantity = quantity;
-	//}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
 
 	public long getSeasoningId() {
 		return seasoningId;
@@ -132,11 +132,11 @@ public class Dish {
 		this.seasoningName = seasoningName;
 	}
 
-	public Ingredient getIngredient() {
-		return ingredient;
-	}
+	//public Ingredient getIngredient() {
+		//return ingredient;
+	//}
 
-	public void setIngredient(Ingredient ingredient) {
-		this.ingredient = ingredient;
-	}
+	//public void setIngredient(Ingredient ingredient) {
+		//this.ingredient = ingredient;
+	//}
 }
