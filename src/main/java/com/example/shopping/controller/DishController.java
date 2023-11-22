66224319c2ff0f5f38624e4d995d78c0dcc30c8c
saya <saya.ishizawa@gmail.com>
2023-11-22 11:04:34 +0900

    軽微な修正

--- src/main/java/com/example/shopping/controller/DishController.java
+++ src/main/java/com/example/shopping/controller/DishController.java
@@ -26,16 +26,16 @@ public class DishController {
 
 	@Autowired
 	DishRepository dishRepository;
-	
+
 	@Autowired
-	IngredientRepository ingredientRepository; //1115
+	IngredientRepository ingredientRepository; // 1115
 
 	@Autowired
 	DishService dishService;
-	
+
 	// 料理一覧画面の表示
 	@GetMapping(value = "/list")
-	public String getList(Dish dish,Model model) {
+	public String getList(Dish dish, Model model) {
 		List<Dish> list = dishService.getList(dish);
 		model.addAttribute("data", list);
 		return "dish/list";
@@ -43,27 +43,28 @@ public class DishController {
 
 	// 料理登録画面の表示
 	@GetMapping(value = "/register")
-	public String register(@ModelAttribute Dish dish, @ModelAttribute("ingredient")Ingredient ingredient,Model model) {
+	public String register(@ModelAttribute Dish dish, @ModelAttribute("ingredient") Ingredient ingredient,
+			Model model) {
 		model.addAttribute("title", "料理の登録");
 		model.addAttribute("dish", new Dish());
 		return "dish/register";
 	}
 
 	// 料理の登録
-	//1115fix
+	// 1115fix
 	@PostMapping(value = "/list")
-	public String add(@ModelAttribute Dish dish, @ModelAttribute("ingredient")Ingredient ingredient,Model model) {
-		//System.out.println("*****" + dish.getIngredient().size());
-		System.out.println(ingredient.getIngredientName());
+	public String add(@ModelAttribute Dish dish, @ModelAttribute Ingredient ingredient, Model model) {
+		// System.out.println("*****" + dish.getIngredient().size());
+		System.out.println(ingredient.getIngredientName());// 登録されている材料の確認用
 		dishRepository.saveAndFlush(dish);
 		String[] ingredientNames = ingredient.getIngredientName().split(",");
 		for (int i = 0; i < ingredientNames.length; i++) {
-		    Ingredient saveName = new Ingredient();
-		    saveName.setIngredientName(ingredientNames[i]);
-		    ingredientRepository.saveAndFlush(saveName);
+			Ingredient saveName = new Ingredient();
+			saveName.setIngredientName(ingredientNames[i]);
+			ingredientRepository.saveAndFlush(saveName);
 		}
 
-		//ingredientRepository.saveAndFlush(ingredient);//1115 材料データの保存
+		// ingredientRepository.saveAndFlush(ingredient);//1115 材料データの保存
 		List<Dish> list = dishService.getList(dish);
 		model.addAttribute("data", list);
 		return "dish/list";
@@ -71,12 +72,13 @@ public class DishController {
 
 	// 登録した料理の詳細表示
 	@GetMapping(value = "detail/{id}")
-	public String getDetail(@PathVariable("id") long dishId, Dish dish,Model model) {
+	public String getDetail(@PathVariable("id") long dishId, Dish dish, Model model) {
 		model.addAttribute("title", "料理の詳細");
 		Optional<Dish> data = dishService.findDish(dishId);
-		//Optional<Ingredient> ingredientData = dishService.findIngredient(ingredientId);//1116追加 //*
+		// Optional<Ingredient> ingredientData =
+		// dishService.findIngredient(ingredientId);//1116追加 //*
 		model.addAttribute("detail", data.get());
-		//model.addAttribute("ingredient-detail",ingredientData.get());//1116追加 //*
+		// model.addAttribute("ingredient-detail",ingredientData.get());//1116追加 //*
 		return "dish/detail";
 	}
 
