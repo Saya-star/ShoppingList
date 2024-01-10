73d0157251a4138304aff2050a20be9a8803f079
saya <saya.ishizawa@gmail.com>
2024-01-10 13:16:58 +0900

    買い物リスト作成のstep2画面にいつも買うものと選択した料理の材料を表示

--- src/main/java/com/example/shopping/controller/ShoppingListController.java
+++ src/main/java/com/example/shopping/controller/ShoppingListController.java
@@ -1,7 +1,9 @@
 package com.example.shopping.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.shopping.entity.AlwaysBuy;
 import com.example.shopping.entity.Dish;
+import com.example.shopping.entity.Ingredient;
 import com.example.shopping.form.SelectForm;
+import com.example.shopping.repository.AlwaysBuyRepository;
 import com.example.shopping.repository.DishRepository;
+import com.example.shopping.repository.IngredientRepository;
 import com.example.shopping.service.DishService;
 
 @Controller
@@ -26,6 +32,12 @@ public class ShoppingListController {
 	
 	@Autowired
 	DishService dishService;
+	
+	@Autowired
+	IngredientRepository ingredientRepository;
+	
+	@Autowired
+	AlwaysBuyRepository alwaysBuyRepository;
 
 	//料理を選択する画面の表示
 	@GetMapping(value="/select1")
@@ -33,7 +45,7 @@ public class ShoppingListController {
 		model.addAttribute("title","買い物リストを作る①");
 		List<Dish> list = dishService.getList(dish);
 		model.addAttribute("data", list);
-		model.addAttribute("SelectForm", new SelectForm());
+//		model.addAttribute("SelectForm", new SelectForm());
 		return "shoppinglist/select1";
 	}
 	
@@ -46,12 +58,31 @@ public class ShoppingListController {
 //	}
 	
 	@PostMapping(value="/select2")
-	public String selectDish(@RequestParam(value = "dishNames", required = false) String[] dishNames, Model model) {
-	    // dishNamesを利用して何かを行う
-	    if (dishNames != null) {
-	        System.out.println(Arrays.toString(dishNames));
-	    }
-	    return "shoppinglist/select2";
+	public String selectDish(@RequestParam(value = "dishIds", required = false) Long[] dishIds, Model model) {
+		// dishNamesを利用して何かを行う
+		if (dishIds != null) {
+			System.out.println(Arrays.toString(dishIds));
+		}
+		//dishIdから材料を検索
+		List<Ingredient> ingredientList = new ArrayList<>();
+		
+		for(int i = 0; i < dishIds.length; i++) {
+			Optional<Dish> selectedDish = dishRepository.findById(dishIds[i]);
+			List<Ingredient> selectedDishIngredient = selectedDish.get().getIngredient();
+			ingredientList.addAll(selectedDishIngredient);
+		}
+		model.addAttribute("ingredientList",ingredientList);
+		
+		
+		//dishIdから調味料を検索
+		
+		//調味料をリストに詰める
+		
+		//いつも買うものリストをリストに詰める
+		List<AlwaysBuy> alwaysBuyList = alwaysBuyRepository.findAll();
+		model.addAttribute("alwaysBuy", alwaysBuyList);
+		//あとで買うものをリストに詰める
+		return "shoppinglist/select2";
 	}
 	
 	@GetMapping(value="/select2")
