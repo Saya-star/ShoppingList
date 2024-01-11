137c993398f68adddd8b880602aede3211049b38
saya <saya.ishizawa@gmail.com>
2024-01-11 14:34:39 +0900

    ShoppingListのエンティティとRepositoryの作成

--- src/main/java/com/example/shopping/entity/ShoppingList.java
+++ src/main/java/com/example/shopping/entity/ShoppingList.java
@@ -1,5 +1,33 @@
 package com.example.shopping.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class ShoppingList {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private long shoppingListId;
+	
+	@Column
+	private long appUserId;
+	
+	@Column
+	private long shopId;
+	
+	@Column
+	private LocalDate createdDate;
+	
+	@Column
+	private boolean shoppingListDeleted = false;
+	
 }
