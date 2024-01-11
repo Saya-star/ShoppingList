756b017638bec188d0cdd454a9703d9371720c13
saya <saya.ishizawa@gmail.com>
2024-01-11 15:01:49 +0900

    買い物リストの材料テーブルのエンティティとレポジトリの追加

--- src/main/java/com/example/shopping/entity/ShoppingList.java
+++ src/main/java/com/example/shopping/entity/ShoppingList.java
@@ -1,12 +1,15 @@
 package com.example.shopping.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -26,6 +29,9 @@ public class ShoppingList {
 	
 	@Column
 	private LocalDate createdDate;
+	
+	@OneToMany(mappedBy="shoppingList",cascade = CascadeType.ALL ) //CascadeType.ALLでいいのか？
+	private List<ShoppingListIngredient> shoppingListIngredients;
 	
 	@Column
 	private boolean shoppingListDeleted = false;
---
+++ src/main/java/com/example/shopping/entity/ShoppingListIngredient.java
@@ -0,0 +1,32 @@
+package com.example.shopping.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ShoppingListIngredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private long shoppingListIngredientId;
+	
+	@Column
+	private long shoppingListId;
+	
+	@Column
+	private long ingredientId;
+	
+	@ManyToOne
+	@JoinColumn(name = "shoppingListId")
+	private ShoppingList shoppingList;
+	
+	@Column
+	private boolean deleted = false;
+	
+}
