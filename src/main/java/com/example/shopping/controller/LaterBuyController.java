7f8533e5e1063f1cd2d8700a72aec7c1fa28786b
saya <saya.ishizawa@gmail.com>
2024-03-14 16:30:56 +0900

    LaterBuyService.javaの作成#9

--- src/main/java/com/example/shopping/controller/LaterBuyController.java
+++ src/main/java/com/example/shopping/controller/LaterBuyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.shopping.entity.LaterBuy;
 import com.example.shopping.repository.LaterBuyRepository;
+import com.example.shopping.service.LaterBuyService;
 
 //あとで買うものリストのページのコントローラー
 @Controller
